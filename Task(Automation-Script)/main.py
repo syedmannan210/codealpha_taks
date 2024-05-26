b954eb138d02cb8d4a6c5c47bed0aedc1c3cbfe7
syedmannan210 <syedmannan210@gmail.com>
2024-05-26 22:43:43 +0500

    Latest changes

--- Task(Automation-Script)/main.py
+++ Task(Automation-Script)/main.py
@@ -18,32 +18,6 @@ def create_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
         print(f"New directory '{directory}' has been created")
-
-# def main():
-#     path = input("Please enter the directory path for File Organization:\n")
-#     while True:
-#         filetype = input('''Enter the file type (e.g., "docx", "pdf", etc.) that you want to organize: \n''')
-#         subdirectory = os.path.join(path, filetype)
-#         if not os.path.exists(subdirectory):
-#             create_directory(subdirectory)
-#             break
-#     custom_loader(0.05)
-#
-#     # Organize files
-#     files = os.listdir(path)
-#     for file in files:
-#         root, extension = os.path.splitext(file)
-#         extension = extension[1:]  # Remove the leading dot
-#         oldpath = os.path.join(path, file)
-#         newpath = os.path.join(path, extension, file)
-#         try:
-#             shutil.move(oldpath, newpath)
-#             print(f"Moved '{file}' to '{newpath}'")
-#             print("Operation Successful!")
-#         except FileNotFoundError:
-#             print(f"Error: File '{file}' not found or destination directory missing.")
-#         except Exception as e:
-#             print(f"Error: {e}")
 def custom_filetype():
     while True:
         filetype = input('''Enter the file type (e.g., "docx", "pdf", etc.) that you want to organize (or type 'exit' to go back):\n''')
